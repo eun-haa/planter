9cd8963e270ba3d3b1af8acb33a26d04399bf831
eunha <GreenArt@DESKTOP-758VK1T>
2022-03-11 14:21:23 +0900

    2022-03-11 PM 02:21 // header.js 수정(슬라이딩 메뉴 적용)

--- planter/src/main/webapp/resources/js/header.js
+++ planter/src/main/webapp/resources/js/header.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
 	
-	// 검색
+	/* 검색 */
 	var headerSearchForm = $("#headerSearchForm");
 	
 	/* 검색 버튼을 누르면 1페이지로 가서 전체 검색이 되게끔 검색 버튼에 클릭 이벤트를 적용 */
@@ -23,28 +23,30 @@ $(document).ready(function(){
 	})
 	
 	//--------------------------------------------------//
-	/* SHOP 버튼 누르면 전체 카테고리 슬라이딩으로 오픈되게 */
-	/*let btn = true;
+	/* SHOP 버튼 누르면 전체 카테고리 닫을 수 있고,다시 클릭하면 열 수 있음 */
+	// if문에 활용할 불린값 정의
+	let btn = false;
 	
+	// 기본값 = 모두 보이게
 	$(".shop_btn").show();
-	$(".shop_record").hide();
+	$(".shop_record").show();
 	
-	
-	
+	// SHOP 버튼을 클릭했을 때
     $(".shop_btn").on("click", function(e){
     	e.preventDefault();
-    	if(btn == true){
-            $(".shop_record").show().animate({
-                bottom:0
-            });
-            btn = false;
-    	}else if(btn == false){
-            $(".shop_record").hide().animate({
-                bottom:0
-            });
+    	if(btn == false){// 불린값이 false이면
+    		// 메뉴를 접는다
+            $(".shop_record").slideUp(150);
+    		
             btn = true;
+    	}else{// 불린값이 true이면(false가 아니면)
+    		// 메뉴를 다시 보여준다
+            $(".shop_record").slideDown(150);
+            btn = false;
     	}
+    	
+    	
 
-    });*/
+    });
 
 })
